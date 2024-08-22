37cf811eec68603d6d9d24a712f750168e21cce5
Ilham Oktavian <99379666+Ilhamokta74@users.noreply.github.com>
2024-08-22 11:13:18 +0700

    fix

--- app.js
+++ app.js
@@ -6,18 +6,10 @@ const PORT = 8000
 const HOST = '0.0.0.0'
 
 const app = express()
-/*
-[ROUTE] '/'
-[Response] => Hello world!
- */
-app.get('/', (req, res) => {
-    res.send('Hello world!\n')
-})
 
-/*
-Optional [ROUTE] '/me'
-[Response] => Dicoding username.
-*/
+app.get('/', (req, res) => {
+    res.send('Hello Ilham Oktavian!\n')
+})
 
 app.listen(PORT, HOST)
 console.log(`Running on http://${HOST}:${PORT}`)
